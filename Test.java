736a451eff976670eef03ab11cda0418313ba821
techlangs <44141121+techlangs@users.noreply.github.com>
2018-10-14 16:33:29 +0000

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+class test 
+{ 
+    public static void main(String args[]) 
+    { 
+        
+        for (x = 0; x < 4; x++) 
+        { 
+            System.out.println(x); 
+        } 
+  
+       System.out.println(x); 
+    } 
+}
